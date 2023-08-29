1b291217e5265f1184f1c4693c05c84196add326
Anna Andrade <annaandrade2307@gmail.com>
2023-08-29 14:33:31 -0400

    Primeiro comt

---
+++ ListdeAluno/Aluno.cs
@@ -0,0 +1,16 @@
+﻿public class Aluno
+{
+    public string Nome { get; set; }
+
+    public int Matricula { get; set; }
+
+    public int Cpf { get; set; }
+
+    public DateOnly Datanasc { get; set; }
+
+    public Aluno()
+    {
+
+    }
+
+}
---
+++ ListdeAluno/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Channels;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        List<Aluno> alunos = new List<Aluno>();
+
+        while (true)
+        {
+            Console.WriteLine("Digite 1 caso queira inserir o nome na lista, caso não queria digite 2");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            if(opcao == 1) 
+            {
+                Aluno a = new Aluno();
+                Console.WriteLine("Irforme o nome, matricula, CPF e data de nascimento do aluno");
+                a.Nome = Console.ReadLine();
+                a.Matricula = Convert.ToInt32(Console.ReadLine());
+                a.Cpf = Convert.ToInt32(Console.ReadLine());
+                a.Datanasc = DateOnly.Parse(Console.ReadLine());
+
+                alunos.Add(a);
+
+
+            }
+            else if(opcao == 2) 
+            {
+                break;
+            }
+
+          
+
+        }
+
+        Console.Clear(); // limpa o console
+        alunos.Sort();    // especificamente para ordenar em ordem alfabetica
+       
+        foreach (Aluno a in alunos)
+        {
+            Console.WriteLine("O nome é " + a.Nome + "o cpf é " + a.Cpf);
+        }
+    }
+}
